b193d847007ba674af13b183dfc30bfe46bc1447
Alina Goaga <goaga.alina@gmail.com>
2018-11-14 16:29:41 +0000

    Added tests for numbers different than 3,5,15 and divisible by 3

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,6 @@
 def fizzbuzz(number)
-  return 'fizz' if number == 3
-  return 'buzz' if number == 5
   return 'fizzbuzz' if number == 15
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number == 5
+  return 1 if number == 1
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -10,4 +10,10 @@ describe 'fizzbuzz' do
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
+  it 'returns 1 when passed 1' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+  it 'returns "fizz" when passed 6' do
+    expect(fizzbuzz(6)).to eq 'fizz'
+  end
 end
